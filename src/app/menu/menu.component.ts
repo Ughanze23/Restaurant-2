d640ccfe166539718d4a88910f894dadf005b9ba
Ughanze Ikenna <ughanzepolycarp@gmail.com>
2017-11-14 14:50:52 +0100

    services

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Dish} from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,11 +10,13 @@ import { DISHES } from '../shared/dishes';
   encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent implements OnInit {
-dishes = DISHES;
+dishes: Dish[];
+
 selectedDish: Dish;
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
  onSelect(dish: Dish) {
    this.selectedDish = dish;
